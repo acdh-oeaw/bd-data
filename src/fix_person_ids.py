3228d895e3efa3383077ad8bb5dbe555e6faf5c7
csae8092 <p.andorfer@gmail.com>
2025-11-28 06:42:49 +0100

    feat: add person label as `@n`

--- src/fix_person_ids.py
+++ src/fix_person_ids.py
@@ -1,15 +1,28 @@
-import glob
+import os
 
 from acdh_tei_pyutils.tei import TeiReader
+from acdh_tei_pyutils.utils import make_entity_label
 
-files = glob.glob("data/editions/*.xml")
+# files = glob.glob("data/editions/*.xml")
 
 
-for x in files:
-    doc = TeiReader(x)
-    for y in doc.any_xpath(".//tei:persName[@ref]"):
-        old_value = y.attrib["ref"]
-        if old_value.startswith("person:"):
-            new_value = old_value.replace("person:", "#bd-")
-            y.attrib["ref"] = new_value
-    doc.tree_to_file(x)
+# for x in files:
+#     doc = TeiReader(x)
+#     for y in doc.any_xpath(".//tei:persName[@ref]"):
+#         old_value = y.attrib["ref"]
+#         if old_value.startswith("person:"):
+#             new_value = old_value.replace("person:", "#bd-")
+#             y.attrib["ref"] = new_value
+#     doc.tree_to_file(x)
+
+
+listperson_file = os.path.join("data", "indices", "listperson.xml")
+
+doc = TeiReader(listperson_file)
+nsmap = doc.nsmap
+
+for x in doc.any_xpath(".//tei:person[@xml:id]"):
+    name = x.xpath("./tei:persName", namespaces=nsmap)[0]
+    label = make_entity_label(name)[0]
+    x.attrib["n"] = label
+doc.tree_to_file(listperson_file)
